bc2b81973b392b9bce69c4dbd6f1ca42e991f380
Eric Mesa <ericsbinaryworld@gmail.com>
2020-03-03 20:31:49 -0500

    Added some notes to myself

--- snapintime/create_local_snapshots.py
+++ snapintime/create_local_snapshots.py
@@ -1,7 +1,15 @@
 """Read in configuration file and create local snapshots."""
 
+import datetime
 import json
 import subprocess
+
+
+def get_date_time():
+    #  remember to use tzdata
+    #  remember to just pass a datetime object because then you can just access like date.year
+    #  rather than what you did last time. HOWEVER, need to make sure hour is formatted 24H time
+    pass
 
 
 def import_config():
@@ -9,6 +17,7 @@ def import_config():
 
 
 def create_snapshot():
+    #  remember to use mroe advanced features of subprocess to know if command failed and to print what it's doing w/o needing to use all those print statements like you did last time.
     pass
 
 
