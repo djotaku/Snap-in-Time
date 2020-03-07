c5eaeea03e372b40f5207f80a186ea4a9578f734
Eric Mesa <ericsbinaryworld@gmail.com>
2020-03-07 15:04:31 -0500

    Should now be able to do a remote send

--- snapintime/tests/test_remote_backup.py
+++ snapintime/tests/test_remote_backup.py
@@ -12,3 +12,41 @@ def test_get_remote_latest_subvol(subprocess_mock):
     subprocess_mock.run.return_value = subprocess_done
     result = remote_backup.get_remote_subvols("user@remote", "/media/backup")
     assert result == ["2020-02-11", "2020-02-17", "2020-02-20", "2020-02-20p2", "2020-02-25", "2020-03-01", "2020-03-05-2022", '']
+
+
+@mock.patch('snapintime.remote_backup.os')
+def test_get_local_subvols(os_mock):
+    mock_return = ["2020-02-11", "2020-02-17", "2020-02-20", "2020-02-20p2", "2020-02-25", "2020-03-01", "2020-03-05-2022"]
+    os_mock.listdir.return_value = mock_return
+    result = remote_backup.get_local_subvols('/dir')
+    assert result == ["2020-02-11", "2020-02-17", "2020-02-20", "2020-02-20p2", "2020-02-25", "2020-03-01", "2020-03-05-2022"]
+
+
+def test_match_subvols_oldest_remote_is_there():
+    """This should not trigger the recursion.
+
+    The final value in the sorted remote list is in the local list.
+    """
+    local_list = ["A", "B", "C", "D"]
+    remote_list = ["A", "B", "C"]
+    result = remote_backup.match_subvols(local_list, remote_list)
+    assert result == "C"
+
+
+def test_match_subvols_oldest_remote_not_there():
+    """This should trigger the recursion.
+
+    The final value in the sorted remote list is not in the local list.
+    """
+    local_list = ["A", "B", "D"]
+    remote_list = ["A", "B", "C"]
+    result = remote_backup.match_subvols(local_list, remote_list)
+    assert result == "B"
+
+
+def test_btrfs_send_receive():
+    pass
+
+
+def test_iterate_configs():
+    pass
