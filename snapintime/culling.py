10ab589e37bcc4879bd2b7555a02fbe6efdc08b7
Eric Mesa <ericsbinaryworld@gmail.com>
2020-03-08 10:42:02 -0400

    Set up irregular test
    This is for if the computer hasn't been on for 24 hours and, therefore,
    doesn't just allow me to remove elements 0, 6, 12, and 18.

--- snapintime/culling.py
+++ snapintime/culling.py
@@ -1,4 +1,22 @@
 """Thin out the snapshots on disk."""
 
 
+def daily_cull(dir_to_cull: list) -> list:
+    """Take a list of snapshots from a directory (already reduced to one day) and cull.
 
+    This culling will produce the closest it can to 4 snapshots\
+    for that day.
+
+    For a perfect set of 24 snapshots, it should leave behind (remove from list):
+
+    - day1-0000
+    - day1-0600
+    - day1-1200
+    - day1-1800
+
+    :param dir_to_cull: A list containing snapshots. Assumes another function\
+    has already reduced this list to a list containing only one day's worth of\
+    snapshots.
+    :returns: A list containing all the subvolumes to cull.
+    """
+    pass
