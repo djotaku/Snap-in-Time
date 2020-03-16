97a3bf9ef88c16eb0557d98c7a69822fcb9007fe
Eric Mesa <ericsbinaryworld@gmail.com>
2020-03-16 16:58:49 -0400

    Increase version number

--- snapintime/__init__.py
+++ snapintime/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Eric Mesa"
-__version__ = "0.9.0"
+__version__ = "0.10.0"
 __license__ = "GNU GPL v3.0"
 __copyright__: str = "(c) 2014 - 2020 Eric Mesa"
 __email__: str = "ericsbinaryworld at gmail dot com"
