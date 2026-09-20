c467352adde2a6f6175a92a00138e4e843cfa8d1
Eric Mesa <ericsbinaryworld@gmail.com>
2026-09-20 16:37:29 -0400

    removing file handlers

--- src/snapintime/__init__.py
+++ src/snapintime/__init__.py
@@ -1,5 +1,5 @@
 __author__ = "Eric Mesa"
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 __license__ = "GNU GPL v3.0"
 __copyright__: str = "(c) 2014 - 2026 Eric Mesa"
 __email__: str = "ericsbinaryworld at gmail dot com"
@@ -13,11 +13,7 @@ log = logging.getLogger("snapintime")
 log.setLevel(logging.INFO)
 RICH_FORMAT = logging.Formatter("%(message)s", "[%X]")
 LOG_FILE_FORMAT = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-file_handler = logging.FileHandler('snap_in_time.log')
-file_handler.setLevel(logging.DEBUG)
-file_handler.setFormatter(LOG_FILE_FORMAT)
 console_handler = RichHandler()
 console_handler.setFormatter(RICH_FORMAT)
-log.addHandler(file_handler)
 log.addHandler(console_handler)
 print(f"Using version {__version__}")
